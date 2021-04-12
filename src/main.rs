fe1cb2981af2f863ba06ae7163e58604b0829e47
Sridhar Ratnakumar <srid@srid.ca>
2021-04-12 18:54:25 -0400

    Use rustbreak to persist data

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use directories::ProjectDirs;
 use exitfailure::ExitFailure;
-use ron::ser::{to_writer_pretty, PrettyConfig};
+use rustbreak::{deser::Ron, PathDatabase};
 use serde::{Deserialize, Serialize};
-use std::fs::File;
+use std::collections::HashMap;
 use std::io::{Error, ErrorKind};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 struct Entity {
     id: String,
 }
@@ -15,16 +15,25 @@ fn main() -> Result<(), ExitFailure> {
         ProjectDirs::from("ca", "srid", "X").ok_or(Error::new(ErrorKind::Other, "No Home"))?;
     let ron_file = proj_dirs.data_dir().with_extension("ron");
 
-    let data = Entity {
-        id: "test".to_string(),
-    };
+    let db = PathDatabase::<HashMap<u32, Entity>, Ron>::load_from_path(ron_file)?;
 
-    println!("Writing to {}", ron_file.display());
-    let pretty = PrettyConfig::new()
-        .with_depth_limit(2)
-        .with_separate_tuple_members(true)
-        .with_enumerate_arrays(true);
-    let fp = File::create(&ron_file)?;
-    let () = to_writer_pretty(fp, &data, pretty.clone())?;
+    db.read(|db| {
+        println!("Init: {:?}", db.get(&0));
+    })?;
+
+    db.write(|db| {
+        db.insert(
+            0,
+            Entity {
+                id: "test".to_string(),
+            },
+        );
+    })?;
+    db.save()?;
+
+    db.read(|db| {
+        println!("Just wrote: {:?}", db.get(&0));
+    })?;
+
     Ok(())
 }
