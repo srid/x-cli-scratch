70235ab15940175748925d48f5bee500ce68f2fe
Sridhar Ratnakumar <srid@srid.ca>
2021-04-12 20:08:57 -0400

    Enable migration; ron->yaml
    Switch from ron serde_yaml because of https://github.com/ron-rs/ron/issues/140

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,73 @@
 use directories::ProjectDirs;
 use exitfailure::ExitFailure;
-use rustbreak::{deser::Ron, PathDatabase};
+use rustbreak::{deser::Yaml, PathDatabase};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::io::{Error, ErrorKind};
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+enum Version {
+    V1,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct Doc {
+    // If data type changes, up the version and write migration.
+    version: Version,
+    data: serde_yaml::Value,
+}
+
+impl Default for Doc {
+    fn default() -> Self {
+        let data: V1Data = HashMap::new();
+        Doc {
+            version: Version::V1,
+            data: serde_yaml::to_value(data).expect("yaml opps"),
+        }
+    }
+}
+
+type V1Data = HashMap<u32, Entity>;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 struct Entity {
     id: String,
 }
 
+fn edit_data<F>(db: &mut Doc, f: F)
+where
+    F: Fn(&mut V1Data) -> (),
+{
+    let val: &mut V1Data = &mut serde_yaml::from_value(db.data.clone()).expect("err");
+    f(val);
+    db.data = serde_yaml::to_value(val).expect("err");
+}
+
 fn main() -> Result<(), ExitFailure> {
     let proj_dirs =
         ProjectDirs::from("ca", "srid", "X").ok_or(Error::new(ErrorKind::Other, "No Home"))?;
-    let ron_file = proj_dirs.data_dir().with_extension("ron");
+    let db_file = proj_dirs.data_dir().with_extension("yaml");
 
-    let db = PathDatabase::<HashMap<u32, Entity>, Ron>::load_from_path(ron_file)?;
+    let db = PathDatabase::<Doc, Yaml>::load_from_path_or_default(db_file)?;
 
     db.read(|db| {
-        println!("Init: {:?}", db.get(&0));
+        println!("Init: {:?}", db.data);
     })?;
 
     db.write(|db| {
-        db.insert(
-            0,
-            Entity {
-                id: "test".to_string(),
-            },
-        );
+        edit_data(db, |val: &mut V1Data| {
+            val.insert(
+                0,
+                Entity {
+                    id: "test".to_string(),
+                },
+            );
+        });
     })?;
     db.save()?;
 
     db.read(|db| {
-        println!("Just wrote: {:?}", db.get(&0));
+        println!("Writ: {:?}", db.data);
     })?;
 
     Ok(())
